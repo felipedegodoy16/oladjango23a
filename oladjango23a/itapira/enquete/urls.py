650e794b2c06c077a59177deb7059fc1ce136d1b
unknown <Godoy>
2023-08-04 10:57:31 -0300

    listagem

--- oladjango23a/itapira/enquete/urls.py
+++ oladjango23a/itapira/enquete/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('felipe', views.caneta, name='caneta'),
     # ex: /enquete/número da questão
-    path("<int:questao_id>/", views.detalhe, name="detalhe"),
+    path("<int:pk>/", views.DetalheView.as_view(), name="detalhe"),
     # ex: /enquete/número da questão/resultados
-    path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
+    path("<int:pk>/resultados/", views.ResultadoView.as_view(), name="resultados"),
     # ex: /enquete/número da questão/voto 
     path("<int:questao_id>/voto/", views.voto, name="voto"),
 ]
--- oladjango23a/itapira/enquete/views.py
+++ oladjango23a/itapira/enquete/views.py
@@ -1,39 +1,48 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Questao, Resposta
 
 
 # Create your views here.
 
-def index(request):
+# def index(request):
     # selecionar os 5 ultimos objetos cadastrados
-    ultimas_questoes = Questao.objects.order_by("data")[:5]
+    # ultimas_questoes = Questao.objects.order_by("data")[:5]
     # criamos um dicionario em python (semelhante aos arrays associativos do php)
     # (ou os arrays objetos literais do javascript) onde passamos essa variavel para ser utilizada no template)
-    context = {'ultimas_questoes': ultimas_questoes}
+    # context = {'ultimas_questoes': ultimas_questoes}
     # retornar a funcao render, passando como argumentos a requisicao,
     # o template que sera utilizado e as variaveis de contexto que serao utilizadas dentro do template
-    return render(request, 'enquete/index.html', context)
+    # return render(request, 'enquete/index.html', context)
     # saida = ", ".join([q.pergunta for q in ultimas_questoes])
     # return HttpResponse(saida)
 
-def caneta(request):
-    return HttpResponse("<h1>Caneta Azul, Azul caneta...</h1>")
+class IndexView(generic.ListView):
+    model = Questao
+    template_name = 'enquete/index.html'
 
-def detalhe(request, questao_id):
+# def detalhe(request, questao_id):
     # try:
     #     questao = Questao.objects.get(pk=questao_id)
     # except Questao.DoesNotExists:
     #     raise Http404("Questao nao ecxisty")
     # faz uma consulta e se não retornar nada levanta um erro 404
     # para comentar pode segurar "ctrl" + "/" ou ";", comenta tudo que você selecionar
-    questao = get_object_or_404(Questao, pk=questao_id)
-    return render(request, 'enquete/detalhe.html', {'questao': questao}) # render pega as informações da requisição(dados de um formulário), pega o template e a variável, renderizar
+    # questao = get_object_or_404(Questao, pk=questao_id)
+    # return render(request, 'enquete/detalhe.html', {'questao': questao}) # render pega as informações da requisição(dados de um formulário), pega o template e a variável, renderizar
 
-def resultados(request, questao_id):
-    questao = get_object_or_404(Questao, pk=questao_id)
-    return render(request, 'enquete/resultado.html', {'questao': questao})
+class DetalheView(generic.DetailView):
+    model = Questao
+    template_name = 'enquete/detalhe.html'
+
+class ResultadoView(generic.DetailView):
+    model = Questao
+    template_name = 'enquete/resultado.html'
+# def resultados(request, questao_id):
+    # questao = get_object_or_404(Questao, pk=questao_id)
+    # return render(request, 'enquete/resultado.html', {'questao': questao})
 
 def voto(request, questao_id):
     questao = get_object_or_404(Questao, pk=questao_id)
